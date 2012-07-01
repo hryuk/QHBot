4e660a93123a60b4545f612c104921ddd099b332
Adrián Pérez Heredia <aperezhrd@gmail.com>
2012-07-01 04:08:29 +0200

    Comentarios añadidos a las clases QHBot y QHBotCommands

--- qhbotcommands.h
+++ qhbotcommands.h
@@ -17,11 +17,23 @@ private:
     QHBotUserManager* UserManager;
     QStringList commands;
 signals:
+    /**
+      \brief Esta señal es emitida siempre que un comando necesite enviar un mensaje de
+      texto a un usuario.
+      **/
     void messageRequest(QString jid,QString msg);
-
     
 public slots:
+    /**
+      \brief Determina si un mensaje contiene un comando ejecutable por el bot.
+
+      Retorna true si el mensaje contiene un comando ejecutable por el bot, false en caso contrario.
+      **/
     bool isCommand(const QXmppMessage& msg);
+
+    /**
+      \brief Ejecuta el comando contenido en el mensaje msg. Responde a la señal QHBot::commandReceived().
+      **/
     bool runCommand(const QXmppMessage& msg);
 
 private slots:
